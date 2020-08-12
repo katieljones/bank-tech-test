4bf33209594e87a2407ffea76099dbd5c3b4dfc7
Katie Jones <katieljones29@gmail.com>
2020-08-12 11:00:10 +0100

    give clearer name to the mapped elements in the statement class for the transactions array

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,7 +2,7 @@ class Statement
 
   def print(transactions)
     head
-    transactions.reverse_each { |s| puts s.gsub(",","||") }
+    transactions.reverse_each { |transaction| puts transaction.gsub(",","||") }
   end
 
   def head
